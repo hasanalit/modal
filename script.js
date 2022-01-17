d3a8c13b2fb6ac006b319e5b5c79b87c52020cdf
hasanalit <tuxtayevhasan72@gmail.com>
2022-01-17 17:00:56 +0500

    modal full

--- script.js
+++ script.js
@@ -5,8 +5,27 @@ let elShowModal = document.querySelector('.show-modal')
 let elModal = document.querySelector('.modal')
 let elCloseModal = document.querySelector('.close-modal')
 let elOverlay = document.querySelector('.overlay')
+elOverlay.style.display = "none"
+elModal.style.display = "none"
+elShowModal.addEventListener('click', function(evt) {
+  elModal.classList.toggle("hidden");
+  elModal.style.display = "inline"
+  elOverlay.style.display = "inline"
+})
 
 document.addEventListener('keydown', function (evt) {
   if (evt.key === 'Escape') {
+    elOverlay.style.display = "none"
+    elModal.style.display = "none"
   }
+});
+
+elCloseModal.addEventListener('click', function (evt) {
+    elOverlay.style.display = "none"
+    elModal.style.display = "none"
+});
+
+elOverlay.addEventListener('click', function (evt) {
+  elOverlay.style.display = "none"
+  elModal.style.display = "none"
 });
