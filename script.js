a93f7ea036c10c899560f4f8acecfcf771cc51e1
hasanalit <tuxtayevhasan72@gmail.com>
2022-01-17 16:32:04 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+let elShowModal = document.querySelector('.show-modal')
+let elModal = document.querySelector('.modal')
+let elCloseModal = document.querySelector('.close-modal')
+let elOverlay = document.querySelector('.overlay')
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Escape') {
+  }
+});
